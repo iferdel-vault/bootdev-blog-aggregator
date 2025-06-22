1be32e80ba06cd048dc5d924c7d0d15ba7f70917
iferdel <ignaferdel@gmail.com>
2025-06-22 18:42:10 -0400

    rss init fetch function

---
+++ rss.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"io"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, nil
+	}
+	req.Header.Set("User-Agent", "gator")
+	c := http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		return &RSSFeed{}, nil
+	}
+	resp.Body.Close()
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &RSSFeed{}, nil
+	}
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(dat, &rssFeed)
+	if err != nil {
+		return &RSSFeed{}, nil
+	}
+
+	return &rssFeed, nil
+}
