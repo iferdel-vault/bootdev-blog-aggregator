ed4a02e5caa37dec8423a3b2caf5e503e5d4b531
iferdel <ignaferdel@gmail.com>
2025-06-21 23:16:50 -0400

    commands into its own file

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"os"
 
@@ -11,38 +9,6 @@ import (
 
 type state struct {
 	cfg *config.Config
-}
-
-type command struct {
-	name string
-	args []string
-}
-
-type commands struct {
-	cmdList map[string]func(*state, command) error
-}
-
-func (cmds *commands) run(s *state, cmd command) error {
-
-	f, ok := cmds.cmdList[cmd.name]
-	if !ok {
-		return errors.New("command does not exists in avilable commands")
-	}
-	err := f(s, cmd)
-	if err != nil {
-		return fmt.Errorf("error running command %s: %w", cmd.name, err)
-	}
-
-	return nil
-}
-
-func (cmds *commands) register(name string, f func(*state, command) error) {
-	_, ok := cmds.cmdList[name]
-	if ok {
-		fmt.Printf("command %s already exists\n", name)
-		return
-	}
-	cmds.cmdList[name] = f
 }
 
 func main() {
