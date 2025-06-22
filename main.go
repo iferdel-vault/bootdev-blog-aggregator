cd746a9e3ad3687e89014cf1be348b65a03a10de
iferdel <ignaferdel@gmail.com>
2025-06-21 22:51:53 -0400

    cmds run and register methods

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
@@ -14,6 +15,33 @@ type state struct {
 type command struct {
 	name string
 	args []string
+}
+
+type commands struct {
+	cmdList map[string]func(*state, command) error
+}
+
+func (cmds *commands) run(s *state, cmd command) error {
+
+	f, ok := cmds.cmdList[cmd.name]
+	if !ok {
+		return errors.New("command does not exists in avilable commands")
+	}
+	err := f(s, cmd)
+	if err != nil {
+		return fmt.Errorf("error running command %s: %w", cmd.name, err)
+	}
+
+	return nil
+}
+
+func (cmds *commands) register(name string, f func(*state, command) error) {
+	_, ok := cmds.cmdList[name]
+	if ok {
+		fmt.Printf("command %s already exists\n", name)
+		return
+	}
+	cmds.cmdList[name] = f
 }
 
 func main() {
