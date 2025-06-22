37b2c86d1d793620028f8bfc35a07c09c08f972e
iferdel <ignaferdel@gmail.com>
2025-06-22 05:26:19 -0400

    add: goose up init

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+CREATE TABLE users (
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMPTZ NOT NULL,
+	updated_at TIMESTAMPTZ NOT NULL,
+	name VARCHAR(50) UNIQUE NULLS NOT DISTINCT
+);
+-- +goose Down
+DROP TABLE users;
